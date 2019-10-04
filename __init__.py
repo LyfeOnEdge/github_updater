f543fc356c23b95783ad3631f5803ab8c219a0b9
Andrew <lyfeshomebrew@gmail.com>
2019-10-04 01:48:15 -0700

    change to json parsing instead of brekan ini parsing

--- __init__.py
+++ __init__.py
@@ -1,9 +1,10 @@
-import configparser, os
+os, json
 from .updater import updater_object
 #get parent directory of parent directory
 parent_parent = os.path.dirname(os.path.dirname(__file__))
-downloader_config = os.path.join(parent_parent, "update.ini")
-parser = configparser.ConfigParser()
-parser.read(downloader_config)
 
-updater = updater_object(parser["DEFAULT"]["UPDATE_NAME"], parser["DEFAULT"]["UPDATEURL"], parser["DEFAULT"]["ASSET_PATTERN"],)
+downloader_config = os.path.join(parent_parent, "updater_settings.json")
+with open (downloader_config) as json_file:
+    downloader_config = json.load(downloader_config)
+
+updater = updater_object(downloader_config["UPDATENAME"], downloader_config["UPDATEURL"], downloader_config["ASSETPATTERN"],)
